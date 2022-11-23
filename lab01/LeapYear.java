c1aa7a000831c87e7cb6d69233d498bd956246f9
mushroom1998 <393474388@qq.com>
2022-11-23 23:24:58 +0800

    lab01 finished

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -9,7 +9,13 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year) {
         // Optional TODO: Fill in this method.
-        return true;
+        if (year%400 == 0) {
+            return true;
+        } else if (year%100 != 0 && year%4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /** Calls isLeapYear to print correct statement. */
